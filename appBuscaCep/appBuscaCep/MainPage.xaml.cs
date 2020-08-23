a3634a0b5f3d9b2793ed441ba16af41faf581280
Rafael Batista <maranata_rafa@hotmail.com>
2020-08-22 21:50:09 -0400

    Projeto concluído

---
+++ appBuscaCep/appBuscaCep/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using appBuscaCep.Models;
+using appBuscaCep.Services;
+using System;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace appBuscaCep
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnBuscar.Clicked += BuscarCep;
+        }
+
+        private void BuscarCep(object sender, EventArgs e)
+        {
+            var cep = txtCep.Text.Trim();
+            if (EhCepValido(cep))
+            {
+                Endereco endereco = ViaCepService.BuscarEnderecoViaCep(cep);
+
+                if (EhEnderecoValido(endereco))
+                {
+                    lblResultado.Text = $"Endereço: {endereco.Localidade} \n" +
+                                        $"UF: {endereco.Uf} \n" +
+                                        $"Bairro: {endereco.Bairro} ";
+                };
+
+                txtCep.Text = string.Empty;
+            }
+        }
+
+        private bool EhCepValido(string cep)
+        {
+            var padraoCep = new Regex(@"\d{5}[\s-.]?\d{3}");
+            var ehCepValido = padraoCep.Match(cep).Success;
+
+            return ehCepValido || AlertaErro("Erro", "Cep digitado incorretamente.");
+        }
+
+        private bool AlertaErro(string titulo, string mensagem)
+        {
+            DisplayAlert(titulo, mensagem, "OK");
+            return false;
+        }
+
+        private bool EhEnderecoValido(Endereco endereco)
+        {
+            if (string.IsNullOrEmpty(endereco.Cep))
+            {
+                DisplayAlert("Erro", $"Endereço não encontrado para o CEP {txtCep.Text}.", "Ok");
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ appBuscaCep/appBuscaCep/Services/ViaCepService.cs
@@ -0,0 +1,31 @@
+﻿using appBuscaCep.Models;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+
+namespace appBuscaCep.Services
+{
+    public class ViaCepService
+    {
+        private static readonly string apiEndereco = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCep(string cep)
+        {
+            Endereco endereco;
+            string novoEnderecoUrl = string.Format(apiEndereco, cep);
+            try
+            {
+                using (var requisicao = new HttpClient())
+                {
+                    var resposta = requisicao.GetStringAsync(novoEnderecoUrl).Result;
+
+                    return endereco = JsonConvert.DeserializeObject<Endereco>(resposta);
+                }
+            }
+            catch (Exception erro)
+            {
+                return null;
+            }
+        }
+    }
+}
